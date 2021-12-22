7f99348bc63a050b5e648d0dcfa8103ef7b32740
chynnalew <chynnalew@yahoo.com>
2021-12-21 22:03:52 -0800

    add function to call api, conditional function to trigger constant api calls

---
+++ scripts.js
@@ -0,0 +1,42 @@
+let quote = [];
+const index = 0;
+let textPosition = 0;
+let flag = true;
+let destination = document.getElementById('text');
+
+function loadQuote() {
+  const url = 'https://api.quotable.io/random';
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log(response.status);
+      }
+    })
+    .then(data => {
+      quote[index] = data.content;
+    })
+}
+
+function displayQuote() {
+  if (flag) {
+    loadQuote();
+    quote[index] += ' ';
+    flag = false;
+  }
+
+  destination.innerHTML = quote[index].substring(0, textPosition) + '<span>\u25AE</span>';
+
+  if (textPosition++ != quote[index].length) {
+    setTimeout('displayQuote()', 100);
+  } else {
+    quote[index] = ' ';
+    setTimeout('displayQuote()', 3000);
+    textPosition = 0;
+    flag = true;
+  }
+}
+
+window.addEventListener('load', displayQuote())
